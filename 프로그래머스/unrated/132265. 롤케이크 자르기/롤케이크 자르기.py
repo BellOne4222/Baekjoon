4f61b6c8a06c3c3352e25cfcd9279b01a920faca
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-08-16 19:13:56 +0900

    [unrated] Title: 롤케이크 자르기, Time: 918.93 ms, Memory: 51.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+def solution(topping):
+    elder = Counter(topping)
+    
+    younger = set()
+    case = 0
+    
+    for i in topping:
+        younger.add(i)
+        elder[i] -= 1
+
+        if elder[i] == 0:
+            del elder[i]
+        if len(younger) == len(elder):
+            case += 1
+
+    return case
+
+
+# print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
